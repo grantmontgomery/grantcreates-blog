e0be602d3ea9b9295da2232b37b65dad64021161
grant <grant@intellipse.com>
2021-06-01 22:52:22 -0700

    rebuild of the directory.

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+
+module.exports = {
+    entry:'./src/index.tsx',
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.[hash].js"
+    },
+   
+}
