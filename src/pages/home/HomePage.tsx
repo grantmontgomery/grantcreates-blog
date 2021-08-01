e9e569f2fbe6065ec61ab8abc62d04911624b351
Grant <gmontgomery1994@gmail.com>
2021-07-31 23:26:25 -0700

    loading line prototype created. changed up global css.

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,12 +3,13 @@ import * as React from "react";
 import css from "./HomePage.module.css";
 
 export const HomePage = () => {
-  console.log(css);
   return (
     <React.Fragment>
       <div className={css.contentWrapper}>
         <main className={css.main}>
-          <section className={css.topArticle}>Grant Creates Blog</section>
+          <section className={css.topArticle}>
+            <div className={css.loadingLine}></div>
+          </section>
           <section className={css.moreArticles}></section>
         </main>
         <aside className={css.articlesAside}></aside>
