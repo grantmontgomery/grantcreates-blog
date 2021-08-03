41fe155c193dbe85b39918d055c349303868017b
Grant <gmontgomery1994@gmail.com>
2021-08-02 23:10:36 -0700

    more article setup.

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -10,12 +10,22 @@ import css from "./Article.module.css";
 
 export const Article: React.FC = () => {
   return (
-    <div className={css.articleLink}>
+    <div className={css.articleWrapper}>
       <div className={css.imageWrapper}>
         <img src="" alt="" />
       </div>
 
-      <div className={css.linksWrapper}></div>
+      <div className={css.linksWrapper}>
+        <a className={css.userLink}>
+          <div className={css.iconWrapper}>
+            <img></img>
+          </div>
+          <p></p>
+        </a>
+        <a className={css.articleLink}>
+          <h3>Title</h3>
+        </a>
+      </div>
       <a>
         <h2></h2>
       </a>
